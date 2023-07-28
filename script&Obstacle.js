8317624dae76b996c57c117b6f70df6da1d0607c
JAZ-CO <jazainaddin@gmail.com>
2023-07-28 08:50:55 +0300

    7/28/2023

--- script&Obstacle.js
+++ script&Obstacle.js
@@ -1,16 +1,25 @@
+
+// store elements needed in variables
 let player = document.getElementById("player");
 let obstacles = document.getElementsByClassName("obstacles");
 let game = document.getElementById("game");
 let wall = document.getElementById("wall");
 let score = document.getElementById("score");
+
+// this variable allow us to increament the score, storing it, and reset it
 let scoreCount = 0;
+
 // function for the obstacles location
 function obstacleVerticalChangeFunc(){
     for(let i =0; i< obstacles.length; i++){
+        // below is just 50/50 for the position of the blue obstacle
+
+        // upper position
         if((Math.random() *100) > 50){
             obstacles[i].style.top = -113 + "px";
     
         }
+        // downward position
         else{
             obstacles[i].style.top = 129 + "px";
         }
@@ -20,12 +29,13 @@ function obstacleVerticalChangeFunc(){
 }
 
 function checkCollisionFunc(){
+    // below is to store each left and top position of the player and obtacle to check the collision between them 
     let chracterTop =parseInt( window.getComputedStyle(player).getPropertyValue("top"));
     let chracterLeft = parseInt( window.getComputedStyle(player).getPropertyValue("left"));
     let blockCollision =parseInt( window.getComputedStyle(obstacles[0]).getPropertyValue("left"));
     let blockTop = parseInt( window.getComputedStyle(obstacles[0]).getPropertyValue("top"));
 
-    
+    // below is basically drawing the conditions manually then apply it here
     if(chracterLeft +50 - blockCollision >=0 &&
         chracterLeft +50 <= blockCollision + 100){
         if(blockTop === -113){
@@ -53,6 +63,7 @@ function scoreFunc(){
     score.innerHTML = "Score: " + scoreCount;
 }
 
+// I made below variables like this so the intervals doesn't start before the game
 let obstacleVerticalChange;
 
 let checkCollision;
